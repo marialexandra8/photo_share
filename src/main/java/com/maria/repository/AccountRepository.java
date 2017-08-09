8f1839400aff323ee38b49d473c25f0f42d32af7
maria-alexandra <alexmary18@yahoo.com>
2017-08-09 21:50:58 +0300

    another try

--- src/main/java/com/maria/repository/AccountRepository.java
+++ src/main/java/com/maria/repository/AccountRepository.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class AccountRepository extends BaseRepository {
     private static AccountRowMapper accountRowMapper = new AccountRowMapper();
 
-    
+
     public Account findById(int id) {
         String sql = "SELECT * FROM accounts WHERE id=?";
         List<Account> accounts = jdbcTemplate.query(sql, new Object[]{id}, accountRowMapper);
